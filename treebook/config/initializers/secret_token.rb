bfdc31c072b24ae89b9446a1723ab82504d46ceb
aself101 <aself@hawaii.edu>
2014-03-27 14:38:36 -1000

    Initial Ruby design
    Basic web app, add, delete, edit to basic database

---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = 'bf46cbb1ab619fdc014115ad0ca242743421b059288f75f5e73c82dbd17511a2ac3327a5ec6764bf1165ec86a54b9b475fdb62a63c0a997f40ccb73f1e835ec4'
